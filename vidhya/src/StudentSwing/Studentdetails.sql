2ccc6fb5173b30cd6a23911281252a3e9364174c
Vidhya2604 <122508406+Vidhya2604@users.noreply.github.com>
2023-11-20 20:46:02 +0530

    Add files via upload

---
+++ vidhya/src/StudentSwing/Studentdetails.sql
@@ -0,0 +1,4 @@
+create database studentreg;
+use studentreg;
+create table student(stu_id varchar(20) primary key, stu_name varchar(20),stu_dept varchar(20),stu_gender varchar(20),stu_phno numeric(10),Eventname varchar(20));
+select * from student;
